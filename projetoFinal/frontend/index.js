6616c1c0674ccc41406c22a107a12b00b6a80a43
Your Name <you@example.com>
2018-11-19 20:52:11 -0200

    jogo da jeva inicial

--- projetoFinal/frontend/index.js
+++ projetoFinal/frontend/index.js
@@ -1,49 +1,49 @@
 class Velha {
 
-    constructor(){
+    constructor() {
         this.eVezDoJogador1 = true;
         this.pontos = [9];
         this.contador = 0;
     }
-   
-    novoJogo(){
-        for (var y = 0;y < 9;y++) {
+
+    novoJogo() {
+        for (var y = 0; y < 9; y++) {
             this.pontos[y] = ""
         }
     }
 
     jogada(position) {
 
-        if (pontos[position] != ""){
+        if (pontos[position] != "") {
             throw new Exception("Já marcado");
         }
 
-        if(eVezDoJogador1){
+        if (eVezDoJogador1) {
             pontos[position] = "X";
-        }else{
+        } else {
             pontos[position] = "O";
         }
 
         eVezDoJogador1 = !eVezDoJogador1;
 
 
-        if(!pontos[0].equals("") && pontos[0].equals(pontos[1].valor) && pontos[0].equals(pontos[2])) throw new NumberFormatException(quemGanhou());
-        if(!pontos[3].equals("") && pontos[3].equals(pontos[4].valor) && pontos[3].equals(pontos[5])) throw new NumberFormatException(quemGanhou());
-        if(!pontos[6].equals("") && pontos[6].equals(pontos[7].valor) && pontos[6].equals(pontos[8])) throw new NumberFormatException(quemGanhou());
-        if(!pontos[0].equals("") && pontos[0].equals(pontos[3].valor) && pontos[0].equals(pontos[5])) throw new NumberFormatException(quemGanhou());
-        if(!pontos[1].equals("") && pontos[1].equals(pontos[4].valor) && pontos[1].equals(pontos[7])) throw new NumberFormatException(quemGanhou());
-        if(!pontos[2].equals("") && pontos[2].equals(pontos[5].valor) && pontos[2].equals(pontos[8])) throw new NumberFormatException(quemGanhou());
-        if(!pontos[0].equals("") && pontos[0].equals(pontos[4].valor) && pontos[0].equals(pontos[8])) throw new NumberFormatException(quemGanhou());
-        if(!pontos[2].equals("") && pontos[2].equals(pontos[4].valor) && pontos[2].equals(pontos[6])) throw new NumberFormatException(quemGanhou());
+        if (!pontos[0].equals("") && pontos[0].equals(pontos[1].valor) && pontos[0].equals(pontos[2])) throw new NumberFormatException(quemGanhou());
+        if (!pontos[3].equals("") && pontos[3].equals(pontos[4].valor) && pontos[3].equals(pontos[5])) throw new NumberFormatException(quemGanhou());
+        if (!pontos[6].equals("") && pontos[6].equals(pontos[7].valor) && pontos[6].equals(pontos[8])) throw new NumberFormatException(quemGanhou());
+        if (!pontos[0].equals("") && pontos[0].equals(pontos[3].valor) && pontos[0].equals(pontos[5])) throw new NumberFormatException(quemGanhou());
+        if (!pontos[1].equals("") && pontos[1].equals(pontos[4].valor) && pontos[1].equals(pontos[7])) throw new NumberFormatException(quemGanhou());
+        if (!pontos[2].equals("") && pontos[2].equals(pontos[5].valor) && pontos[2].equals(pontos[8])) throw new NumberFormatException(quemGanhou());
+        if (!pontos[0].equals("") && pontos[0].equals(pontos[4].valor) && pontos[0].equals(pontos[8])) throw new NumberFormatException(quemGanhou());
+        if (!pontos[2].equals("") && pontos[2].equals(pontos[4].valor) && pontos[2].equals(pontos[6])) throw new NumberFormatException(quemGanhou());
 
         let jogoAcabou = true;
-        for (let x = 0;x < 9; x++){
-            if (pontos[x] == ""){
+        for (let x = 0; x < 9; x++) {
+            if (pontos[x] == "") {
                 jogoAcabou = false;
             }
         }
 
-        if(jogoAcabou){
+        if (jogoAcabou) {
             throw new Exception("Game over!");
         }
 
@@ -53,9 +53,9 @@ class Velha {
     }
 
     quemGanhou() {
-        if(eVezDoJogador1){
+        if (eVezDoJogador1) {
             return "Jogador 1 Ganhou!";
-        }else{
+        } else {
             return "Jogador 2 Ganhou!";
         }
 
@@ -66,10 +66,14 @@ class Velha {
 let velha = new Velha()
 velha.novoJogo()
 
-function render(){
-    
-    for (var i; i++; velha.pontos.length > 0){
-        let elemento = document.querySelector(".val"+i)
+function acao(pos) {
+    velha.jogada(pos)
+}
+
+function render() {
+
+    for (var i; i < velha.pontos.length; i++) {
+        let elemento = document.querySelector(".val" + i)
         var val = 'none'
         if (velha.pontos === "X") {
             val = 'times'
@@ -77,8 +81,8 @@ function render(){
         if (velha.pontos === "O") {
             val = 'circle'
         }
-        var val = 'circle' 
-        elemento.innerHTML = '<i class="fa fa-'+val+' elemento"></i>';
+        var val = 'circle'
+        elemento.innerHTML = '<i onClick="acao(' + i + ')" class="fa fa-' + val + ' elemento"></i>';
     }
 
 }
